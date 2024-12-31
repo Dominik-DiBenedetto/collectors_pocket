1f24353edd4edda14b058703c85805431e308a2d
Dom <thescriptinglegend@gmail.com>
2024-12-30 20:30:18 -0500

    database init

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -9,7 +9,7 @@ async fn main() -> std::io::Result<()> {
     let subscriber = telemetry::get_subscriber(settings.clone().debug);
     telemetry::init_subscriber(subscriber);
 
-    let application = startup::Application::build(settings).await?;
+    let application = backend::startup::Application::build(settings, None).await?;
 
     tracing::event!(target: "backend", tracing::Level::INFO, "Listening on http://127.0.0.1:{}/", application.port());
 
